95400febd500a7d8e3434df35449c702b24f792d
Jay Mistry <90444018+Cipher-935@users.noreply.github.com>
2024-06-10 18:00:11 -0700

    Add files via upload

--- middlewares/redis_client.js
+++ middlewares/redis_client.js
@@ -1,8 +1,11 @@
 const {Redis} = require("ioredis");
 
-const cluster_endpoint = "chat-app-cluster-rq1dr5.serverless.use2.cache.amazonaws.com";
+const cluster_endpoint = "resonance-cache-rq1dr5.serverless.use2.cache.amazonaws.com";
 const cluster_port = 6379;
 
-const client = new Redis();
+const client = new Redis({
+    host: cluster_endpoint,
+    port: 6379
+});
 
 module.exports = client;
